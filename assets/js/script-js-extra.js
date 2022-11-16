96d9dc3f4c4c51307cc4203eaa10b11168f4c321
Yoshio Onuki <105018988+YoshioOnuki@users.noreply.github.com>
2022-11-16 01:36:35 -0500

    Commit LandingPage-VentaGorras

---
+++ assets/js/script-js-extra.js
@@ -0,0 +1,68 @@
+var DIVI = {
+    "item_count": "%d Item",
+    "items_count": "%d Items"
+};
+var et_builder_utils_params = {
+    "condition": {
+        "diviTheme": true,
+        "extraTheme": false
+    },
+    "scrollLocations": ["app", "top"],
+    "builderScrollLocations": {
+        "desktop": "app",
+        "tablet": "app",
+        "phone": "app"
+    },
+    "onloadScrollLocation": "app",
+    "builderType": "fe"
+};
+var et_frontend_scripts = {
+    "builderCssContainerPrefix": "#et-boc",
+    "builderCssLayoutPrefix": "#et-boc .et-l"
+};
+var et_pb_custom = {
+    "ajaxurl": "https:\/\/www.elegantthemes.com\/layouts\/wp-admin\/admin-ajax.php",
+    "images_uri": "https:\/\/www.elegantthemes.com\/layouts\/wp-content\/themes\/Divi\/images",
+    "builder_images_uri": "https:\/\/www.elegantthemes.com\/layouts\/wp-content\/themes\/Divi\/includes\/builder\/images",
+    "et_frontend_nonce": "f6713a049f",
+    "subscription_failed": "Please, check the fields below to make sure you entered the correct information.",
+    "et_ab_log_nonce": "49be6fc036",
+    "fill_message": "Please, fill in the following fields:",
+    "contact_error_message": "Please, fix the following errors:",
+    "invalid": "Invalid email",
+    "captcha": "Captcha",
+    "prev": "Prev",
+    "previous": "Previous",
+    "next": "Next",
+    "wrong_captcha": "You entered the wrong number in captcha.",
+    "wrong_checkbox": "Checkbox",
+    "ignore_waypoints": "no",
+    "is_divi_theme_used": "1",
+    "widget_search_selector": ".widget_search",
+    "ab_tests": [],
+    "is_ab_testing_active": "",
+    "page_id": "213047",
+    "unique_test_id": "",
+    "ab_bounce_rate": "5",
+    "is_cache_plugin_active": "no",
+    "is_shortcode_tracking": "",
+    "tinymce_uri": "https:\/\/www.elegantthemes.com\/layouts\/wp-content\/themes\/Divi\/includes\/builder\/frontend-builder\/assets\/vendors",
+    "accent_color": "#7EBEC5",
+    "waypoints_options": []
+};
+var et_pb_box_shadow_elements = [];
+
+
+var wc_cart_fragments_params = {
+    "ajax_url": "\/layouts\/wp-admin\/admin-ajax.php",
+    "wc_ajax_url": "\/layouts\/?wc-ajax=%%endpoint%%",
+    "cart_hash_key": "wc_cart_hash_97021550e9eab2d14fdb9c3ac6541ec6",
+    "fragment_name": "wc_fragments_97021550e9eab2d14fdb9c3ac6541ec6",
+    "request_timeout": "5000"
+};
+
+
+var woocommerce_params = {
+    "ajax_url": "\/layouts\/wp-admin\/admin-ajax.php",
+    "wc_ajax_url": "\/layouts\/?wc-ajax=%%endpoint%%"
+};
