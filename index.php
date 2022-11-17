043e92c79b8e1ab05f005945ba375730432256ff
Yoshio Onuki <105018988+YoshioOnuki@users.noreply.github.com>
2022-11-16 22:44:23 -0500

    Fondo - php

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 	
 <head>
 	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Venta de Gorras</title>
 
@@ -11,23 +10,13 @@
 	
 	<link href="assets/css/style.css" rel="stylesheet">
 	<link href="assets/css/style2.css" rel="stylesheet">
-
-	<script src="assets/js/jquery.min.js"></script>
-	<script src="assets/js/jquery-migrate.min.js"></script>
-
-	<noscript>
-		<style>
-			.woocommerce-product-gallery {
-				opacity: 1 !important;
-			}
-		</style>
-	</noscript>
+	<link href="assets/css/woocommerce-layout.css" rel="stylesheet">
+	<link href="assets/css/woocommerce-smallscreen.css" rel="stylesheet">
+	<link href="assets/css/woocommerce.css" rel="stylesheet">
 </head>
 
 <body class="page-template-default page page-id-213047 theme-Divi woocommerce-no-js et_pb_button_helper_class et_fullwidth_nav et_fixed_nav et_show_nav et_primary_nav_dropdown_animation_fade et_secondary_nav_dropdown_animation_fade et_header_style_left et_pb_footer_columns4 et_cover_background et_pb_gutter windows et_pb_gutters3 et_pb_pagebuilder_layout et_no_sidebar et_divi_theme et-db">
-
 	<div id="page-container">
-		
 		<!-- Encabezado -->
 		<header id="main-header" data-height-onload="66">
 			<div class="container clearfix et_menu_container">
@@ -66,10 +55,11 @@
 					<div class="entry-content">
 						<div class="et-l et-l--post">
 							<div class="et_builder_inner_content et_pb_gutters3">
-
 								<!-- Home -->
 								<div class="et_pb_section et_pb_section_0 et_pb_section_parallax et_pb_with_background et_section_regular section_has_divider et_pb_bottom_divider">
-									<span class="et_parallax_bg_wrap"><span class="et_parallax_bg" style="background-image: url(https://images.hdqwalls.com/wallpapers/abstract-fractal-digital-art-yo.jpg);"></span></span>
+									<span class="et_parallax_bg_wrap">
+										<span class="et_parallax_bg fondHeFoo"></span>
+									</span>
 									<div class="et_pb_row et_pb_row_0">
 										<div class="et_pb_column et_pb_column_4_4 et_pb_column_0  et_pb_css_mix_blend_mode_passthrough et-last-child">
 											<div class="et_pb_module et_pb_text et_pb_text_0  et_pb_text_align_center et_pb_bg_layout_dark">
@@ -98,7 +88,6 @@
 													<div class="et_pb_blurb_container">
 														<h5><span>Gorra para Hombre REEF CAP REEF TOWN MLTY</span></h5>
 														<div class="et_pb_blurb_description">
-															<!-- <h5>Gorra para Hombre REEF CAP REEF TOWN MLTY</h5> -->
 															<b style="color: red;">S/.39.90</b><br>
 															<del>S/.79.90</del>
 														</div>
@@ -117,7 +106,6 @@
 													<div class="et_pb_blurb_container">
 														<h5><span>Gorra para Hombre ADIDAS</span></h5>
 														<div class="et_pb_blurb_description">
-															<!-- <h5>Gorra para Hombre REEF CAP REEF TOWN MLTY</h5> -->
 															<b style="color: red;">S/.49.90</b><br>
 															<del>S/.79.90</del>
 														</div>
@@ -127,7 +115,7 @@
 										</div>
 									</div>
 									<div class="et_pb_row et_pb_row_2">
-										<div class="et_pb_column et_pb_column_1_2 et_pb_column_3  et_pb_css_mix_blend_mode_passthrough">
+										<div class="et_pb_column et_pb_column_1_2 et_pb_column_3 et_pb_css_mix_blend_mode_passthrough">
 											<div class="et_pb_module et_pb_blurb et_pb_blurb_2  et_pb_text_align_left  et_pb_blurb_position_top et_pb_bg_layout_light">
 												<div class="et_pb_blurb_content">
 													<div class="et_pb_main_blurb_image">
@@ -138,7 +126,6 @@
 													<div class="et_pb_blurb_container">
 														<h5><span>Gorra para Hombre DC SHOES CAP BYJ0</span></h5>
 														<div class="et_pb_blurb_description">
-															<!-- <h5>Gorra para Hombre REEF CAP REEF TOWN MLTY</h5> -->
 															<b style="color: red;">S/.59.90</b><br>
 															<del>S/.99.90</del>
 														</div>
@@ -146,7 +133,7 @@
 												</div>
 											</div>
 										</div>
-										<div class="et_pb_column et_pb_column_1_2 et_pb_column_4  et_pb_css_mix_blend_mode_passthrough et-last-child">
+										<div class="et_pb_column et_pb_column_1_2 et_pb_column_4 et_pb_css_mix_blend_mode_passthrough et-last-child">
 											<div class="et_pb_module et_pb_blurb et_pb_blurb_3 et_animated  et_pb_text_align_left  et_pb_blurb_position_top et_pb_bg_layout_light">
 												<div class="et_pb_blurb_content">
 													<div class="et_pb_main_blurb_image">
@@ -157,7 +144,6 @@
 													<div class="et_pb_blurb_container">
 														<h5><span>Gorra para Hombre VANS azul</span></h5>
 														<div class="et_pb_blurb_description">
-															<!-- <h5>Gorra para Hombre REEF CAP REEF TOWN MLTY</h5> -->
 															<b style="color: red;">S/.59.90</b><br>
 															<del>S/.69.90</del>
 														</div>
@@ -178,7 +164,6 @@
 													<div class="et_pb_blurb_container">
 														<h5><span>Gorra para Hombre VANS rojo</span></h5>
 														<div class="et_pb_blurb_description">
-															<!-- <h5>Gorra para Hombre REEF CAP REEF TOWN MLTY</h5> -->
 															<b style="color: red;">S/.49.90</b><br>
 															<del>S/.69.90</del>
 														</div>
@@ -197,7 +182,6 @@
 													<div class="et_pb_blurb_container">
 														<h5><span>Gorra para Hombre Mickey Mouse</span></h5>
 														<div class="et_pb_blurb_description">
-															<!-- <h5>Gorra para Hombre REEF CAP REEF TOWN MLTY</h5> -->
 															<b style="color: red;">S/.39.90</b><br>
 															<del>S/.59.90</del>
 														</div>
@@ -218,7 +202,6 @@
 													<div class="et_pb_blurb_container">
 														<h5><span>Gorra para Hombre Rayo</span></h5>
 														<div class="et_pb_blurb_description">
-															<!-- <h5>Gorra para Hombre REEF CAP REEF TOWN MLTY</h5> -->
 															<b style="color: red;">S/.39.90</b><br>
 															<del>S/.59.90</del>
 														</div>
@@ -237,7 +220,6 @@
 													<div class="et_pb_blurb_container">
 														<h5><span>Gorra para Hombre CHICAGO BULLS</span></h5>
 														<div class="et_pb_blurb_description">
-															<!-- <h5>Gorra para Hombre REEF CAP REEF TOWN MLTY</h5> -->
 															<b style="color: red;">S/.59.90</b><br>
 															<del>S/.89.90</del>
 														</div>
@@ -276,8 +258,9 @@
 								<!-- Contacto -->
 								<div class="et_pb_section et_pb_section_2 et_pb_section_parallax et_pb_with_background et_section_regular section_has_divider et_pb_bottom_divider et_pb_top_divider" id="foo">
 									<div class="et_pb_top_inside_divider et-no-transition"></div>
-									<span class="et_parallax_bg_wrap"><span class="et_parallax_bg"
-											style="background-image: url(https://images.hdqwalls.com/wallpapers/abstract-fractal-digital-art-yo.jpg);"></span></span>
+									<span class="et_parallax_bg_wrap">
+										<span class="et_parallax_bg fondHeFoo"></span>
+									</span>
 									<div class="et_pb_row et_pb_row_4 et_pb_row_fullwidth" id="contacto">
 										<div class="et_pb_column et_pb_column_4_4 et_pb_column_7  et_pb_css_mix_blend_mode_passthrough et-last-child">
 											<div class="et_pb_module et_pb_text et_pb_text_1  et_pb_text_align_center et_pb_bg_layout_dark">
@@ -318,74 +301,8 @@
 		</div>
 	</div>
 
-	<script type="text/javascript">
-		var et_animation_data = [{
-			"class": "et_pb_blurb_0",
-			"style": "zoom",
-			"repeat": "once",
-			"duration": "1000ms",
-			"delay": "0ms",
-			"intensity": "10%",
-			"starting_opacity": "0%",
-			"speed_curve": "ease-in-out"
-		}, {
-			"class": "et_pb_blurb_1",
-			"style": "zoom",
-			"repeat": "once",
-			"duration": "1000ms",
-			"delay": "0ms",
-			"intensity": "10%",
-			"starting_opacity": "0%",
-			"speed_curve": "ease-in-out"
-		}, {
-			"class": "et_pb_blurb_3",
-			"style": "zoom",
-			"repeat": "once",
-			"duration": "1000ms",
-			"delay": "0ms",
-			"intensity": "10%",
-			"starting_opacity": "0%",
-			"speed_curve": "ease-in-out"
-		}, {
-			"class": "et_pb_blurb_4",
-			"style": "zoom",
-			"repeat": "once",
-			"duration": "1000ms",
-			"delay": "0ms",
-			"intensity": "10%",
-			"starting_opacity": "0%",
-			"speed_curve": "ease-in-out"
-		}, {
-			"class": "et_pb_blurb_5",
-			"style": "zoom",
-			"repeat": "once",
-			"duration": "1000ms",
-			"delay": "0ms",
-			"intensity": "10%",
-			"starting_opacity": "0%",
-			"speed_curve": "ease-in-out"
-		}];
-	</script>
-
-	<script type="text/javascript">
-		(function () {
-			var c = document.body.className;
-			c = c.replace(/woocommerce-no-js/, 'woocommerce-js');
-			document.body.className = c;
-		})();
-	</script>
-
-	<script type='text/javascript' id='wc-add-to-cart-js-extra'>
-		var wc_add_to_cart_params = {
-			"ajax_url": "\/layouts\/wp-admin\/admin-ajax.php",
-			"wc_ajax_url": "\/layouts\/?wc-ajax=%%endpoint%%",
-			"i18n_view_cart": "View cart",
-			"cart_url": "https:\/\/www.elegantthemes.com\/layouts\/cart",
-			"is_cart": "",
-			"cart_redirect_after_add": "no"
-		};
-	</script>
-
+	<script src="assets/js/jquery.min.js"></script>
+	<script src="assets/js/jquery-migrate.min.js"></script>
 	<script src="assets/js/js.cookie.min.js"></script>
 	<script src="assets/js/query.blockUI.min.js"></script>
 	<script src="assets/js/add-to-cart.min.js"></script>
@@ -393,7 +310,6 @@
 	<script src="assets/js/script-js-extra.js"></script>
 	<script src="assets/js/scripts.min.js"></script>
 	<script src="assets/js/beacon.min.js"></script>
-
 </body>
 
 </html>
